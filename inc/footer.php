6fa3ac214a53f9fc74dfb2c9ae3e20e3de3a3851
iley24 <iley.lebie@gmail.com>
2025-04-07 12:17:11 +0200

    Ajout du dossier inc

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+<footer>&copy; copyright - ILEY - MEWAN - ADAM</footer>
+<script type="text/javascript" src="<?= BASE ?>js/burger.js"></script>
+<script type="text/javascript" src="<?= BASE ?>js/lightbox.js"></script>
+</body>
+</html>
