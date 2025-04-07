fa11312e55918527235f8ae8e5328a4b9bc4b680
iley24 <iley.lebie@gmail.com>
2025-04-07 11:59:53 +0200

    ajout de l'index

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+$title = 'Accueil';
+include '<inc/header.php' ?>
+<main>
+    
+    <section>
+    
+<h2>Bienvenue sur BMW Dream 🚗✨</h2>
+<br>
+Vous rêvez de posséder une BMW ? Avec BMW Dream, tentez votre chance pour gagner la voiture de vos rêves !<br>
+<br>
+📝 Comment ça marche ?<br>
+<br>
+<ul>
+    <li>1️⃣ Inscrivez-vous en indiquant votre nom et le modèle BMW de vos rêves dans l'onglet Inscriptions.</li><br>
+    <li>2️⃣ Retrouvez votre nom dans l'onglet Liste, aux côtés des autres participants.</li><br>
+    <li>3️⃣ Si vous êtes sélectionné, vous remportez la voiture que vous avez choisie !🎉</li><br>
+</ul>
+🚀 Ne manquez pas cette opportunité unique ! Inscrivez-vous dès maintenant et tentez de gagner votre BMW préférée.<br>
+<br>
+Bonne chance ! 🍀
+    </section>
+    <aside>
+        <h2>Les modèles les plus appréciés : </h2>
+        <img src='image/bmwm4.JPG' alt="bmw M4">
+        <img src='image/bmwm3.JPG' alt="bmw M3">
+        <img src='image/bmwi8.JPG' alt="bmw I8">
+        <img src='image/bmwx7.JPG' alt="bmw X7s">
+        <img src='image/bmwm5.JPG' alt="bmw M5">
+        <img src='image/bmwz4.JPG' alt="bmw Z4">
+    </aside>
+</main>
+
+<?php include '<inc/footer.php' ?>
