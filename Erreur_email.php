b902ef0a6837656010a14b16d1c12d472c848c3d
M3WAN <mewan.jouan@gmail.com>
2025-04-07 14:09:13 +0200

    dépot de erreur mail fait

---
+++ Erreur_email.php
@@ -0,0 +1,31 @@
+<?php
+$title = 'Inscriptions';
+include '../inc/header.php' ?>
+
+<main>
+<section>
+    
+<h2>Inscrivez-vous pour tenter votre chance !</h2>
+<br>
+Remplissez le formulaire ci-dessous et entrez dans la course pour gagner la BMW de vos rêves !  
+<br><br>
+
+📝 Comment s'inscrire ?<br>
+<br>
+<ul>
+    <li>1️⃣ Indiquez votre nom et le modèle BMW que vous souhaitez gagner.</li><br>
+    <li>2️⃣ Validez votre inscription en soumettant le formulaire.</li><br>
+    <li>3️⃣ Consultez la liste des participants dans l'onglet Commandes.</li><br>
+</ul>
+
+📢 Le tirage au sort désignera le grand gagnant ! 
+N'attendez plus, inscrivez-vous dès maintenant et tentez votre chance ! 🍀  
+
+</section>
+<aside>
+    <h3>Vous êtes déjà inscrit à ce concours avec cette adresse email.<h3>
+</aside>
+</main>
+
+
+<?php include '../inc/footer.php' ?>
